6cfba8c83a901f5a8a1d92fb64e40958b845e2c3
Nho89 <noa1720@gmail.com>
2024-02-21 14:45:31 +0100

    Outlet

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
   return (
     <>
     <nav></nav>
-
+    <Outlet/>
     <footer></footer>
     </>
   )
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
+import Home from "../views/Home";
 
 const router = createBrowserRouter([
     {
         path: "/",
       element: <Layout/>,
       children:[
-        {},{}
+        {
+            path: "/",
+            element: <Home/>,
+        },{}
       ]
 
     } /* add routes here */,
