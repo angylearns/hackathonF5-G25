cd50c1d22778a1773b015e93ac5a178231a9cfc3
Nho89 <noa1720@gmail.com>
2024-02-21 15:38:53 +0100

    Home routes

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -8,7 +8,7 @@ const router = createBrowserRouter([
       element: <Layout/>,
       children:[
         {
-            path: "Home",
+            path: "/",
             element: <Home/>,
         },{}
       ]
