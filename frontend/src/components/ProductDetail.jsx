158c5a2d39a1b4f0d2c3c21c16a718c8bff6247f
Nho89 <noa1720@gmail.com>
2024-02-22 11:34:08 +0100

    css product detail

--- frontend/src/components/ProductDetail.jsx
+++ frontend/src/components/ProductDetail.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {useParams} from 'react-router-dom'
 import {getProductById} from '../services/services'
+import './ProductDetail.css'
 
 const ProductDetail = () => {
   const {id} = useParams();
@@ -20,14 +21,23 @@ const ProductDetail = () => {
   }
 
   return (
-    <div>
-       <div className='container-product-id'>
-          <h1>{product.name}</h1>
-          <img src={product.image} alt={product.name} />
-          <p>{product.description}</p>
-          <p>Price: {product.price}</p>
+    <>
+        <div className='container-product-id'>
+            <div>
+                <h1>{product.name}</h1>
+                <img src={product.image} alt={product.name} />
+                
+            </div>
+          
+        
+            <div>
+                <p>{product.location_id}</p>
+                <p>Price: {product.price}</p>
+                <p>{product.description}</p>
+            </div>
         </div>
-    </div>
+    </>
+       
   );
 };
 
